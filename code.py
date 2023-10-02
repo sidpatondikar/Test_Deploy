f6647b29d350ca8837036d1437be5ca2c236ac69
Siddharth Patondikar <83869822+sidpatondikar@users.noreply.github.com>
2023-10-02 18:21:10 +0530

    commit

--- code.py
+++ code.py
@@ -16,21 +16,23 @@ with open('scaler.pkl', 'rb') as scaler_file:
 if 'step' not in st.session_state:
     st.session_state.step = 1
 
+# Initialize input variables with default values
+age, sex, education, cigs_per_day, bp_meds, prevalent_stroke, prevalent_hyp, diabetes = (30, 'Male', 1, 0, 'No', 'No', 'No', 'No')
+
 # Function to reset the step to 1
 def reset_step():
     st.session_state.step = 1
 
 # Define input fields
-st.title(':blue[Heart Disease] :red[Risk] :blue[Prediction] :heart:')
-st.write('Double click :red[`Restart`] button to start the process again')
+st.title(':blue[Heart Disease] :red[Risk] :blue[Prediction]')
 st.write('**Please provide the following information**')
 
 # Step 1: Demographic data
 if st.session_state.step == 1:
     st.write('## :green[Demographic Data]')
-    age = st.slider('**Age**', 1, 100, 30)
-    sex = st.radio('**Sex**', ['Male', 'Female'])
-    education = st.selectbox('**Education Level**', [1, 2, 3, 4])
+    age = st.slider('**Age**', 1, 100, age)
+    sex = st.radio('**Sex**', ['Male', 'Female'], sex)
+    education = st.selectbox('**Education Level**', [1, 2, 3, 4], education)
 
     # Add Behavioral Data button
     if st.button('Add Behavioral Data'):
@@ -39,7 +41,7 @@ if st.session_state.step == 1:
 # Step 2: Behavioral Data
 if st.session_state.step == 2:
     st.write('## :green[Behavioral Data]')
-    cigs_per_day = st.number_input('**Cigarettes Per Day**', min_value=0, max_value=50, value=0, step=1)
+    cigs_per_day = st.number_input('**Cigarettes Per Day**', min_value=0, max_value=50, value=cigs_per_day, step=1)
 
     # Add Medical History button
     if st.button('Add Medical History'):
@@ -48,10 +50,10 @@ if st.session_state.step == 2:
 # Step 3: Medical history
 if st.session_state.step == 3:
     st.write('## :green[Medical History]')
-    bp_meds = st.radio('**Taking Blood Pressure Medication?**', ['Yes', 'No'])
-    prevalent_stroke = st.radio('**Prevalent Stroke?**', ['Yes', 'No'])
-    prevalent_hyp = st.radio('**Prevalent Hypertension?**', ['Yes', 'No'])
-    diabetes = st.radio('**Diabetes?**', ['Yes', 'No'])
+    bp_meds = st.radio('**Taking Blood Pressure Medication?**', ['Yes', 'No'], bp_meds)
+    prevalent_stroke = st.radio('**Prevalent Stroke?**', ['Yes', 'No'], prevalent_stroke)
+    prevalent_hyp = st.radio('**Prevalent Hypertension?**', ['Yes', 'No'], prevalent_hyp)
+    diabetes = st.radio('**Diabetes?**', ['Yes', 'No'], diabetes)
 
     # Add Medical Current button
     if st.button('Add Medical Current'):
@@ -68,9 +70,16 @@ if st.session_state.step == 4:
 
     # Predict button
     if st.button(':green[Predict]'):
+        # Convert sex, bp_meds, prevalent_stroke, prevalent_hyp, and diabetes to integers
+        sex_int = 1 if sex == 'Male' else 0
+        bp_meds_int = 1 if bp_meds == 'Yes' else 0
+        prevalent_stroke_int = 1 if prevalent_stroke == 'Yes' else 0
+        prevalent_hyp_int = 1 if prevalent_hyp == 'Yes' else 0
+        diabetes_int = 1 if diabetes == 'Yes' else 0
+
         # Create a numpy array with input values
-        input_values = np.array([age, education, sex == 'Male', cigs_per_day, bp_meds == 'Yes',
-                                prevalent_stroke == 'Yes', prevalent_hyp == 'Yes', diabetes == 'Yes',
+        input_values = np.array([age, education, sex_int, cigs_per_day, bp_meds_int,
+                                prevalent_stroke_int, prevalent_hyp_int, diabetes_int,
                                 tot_chol, bmi, heart_rate, glucose, pulse_pressure])
 
         # Scale the input values using the loaded scaler
@@ -87,8 +96,3 @@ if st.session_state.step == 4:
 # Restart button
 if st.button('Restart', key='restart'):
     reset_step()
-
-#Footer
-st.write(':blue[**Cardiovascular**] :red[**Risk**] :blue[**Prediction**], based on tuned Random Forest model')
-st.write('Checkout the [repository](https://github.com/sidpatondikar/Capstone-Cardiovascular-Risk-Prediction) for model build')
-st.write('ML Model and Deployment by: :blue[**Siddharth Patondikar**]')
